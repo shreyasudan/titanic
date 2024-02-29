1f077a5157ef9ee1fc80fabce20f0f9a891195c7
Shreya Sudan <120227055+shreyasudan@users.noreply.github.com>
2024-02-29 15:27:56 -0800

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,64 @@
 <script>
-  // Write your JS here, or import other files
+  import * as d3 from 'd3';
+    
+  import { onMount } from 'svelte';
+
+    let titanicData = [];
+    
+    onMount(async () => {
+
+        const res = await fetch('Titanic-Data.csv'); 
+
+        const csv = await res.text();
+
+        emissionData = d3.csvParse(csv, d3.autoType)
+
+        $: console.log(titanicData);
+        createBubbleChart();
+
+    });
+
 </script>
 
-<main>
-  <h1>Svelte template</h1>
+function createBubbleChart() {
+    const svg = d3.select("svg");
+    const width = +svg.attr("width");
+    const height = +svg.attr("height");
 
-  <p>Write your HTML here</p>
-</main>
+    // Define scales
+    const xScale = d3.scaleLinear()
+                     .domain(d3.extent(titanicData, d => d.Age))
+                     .range([0, width]);
+                     
+    const yScale = d3.scaleLinear()
+                     .domain(d3.extent(titanicData, d => d.Fare))
+                     .range([height, 0]);
+                     
+    const colorScale = d3.scaleOrdinal()
+                         .domain([0, 1])
+                         .range(["red", "green"]);
 
-<style>
-  /* Write your CSS here */
-</style>
+    // Add circles
+    svg.selectAll(".point")
+       .data(titanicData)
+       .enter().append("circle")
+       .attr("class", "point")
+       .attr("cx", d => xScale(d.Age))
+       .attr("cy", d => yScale(d.Fare))
+       .attr("r", 5) // Radius of circles
+       .attr("fill", d => colorScale(d.Survived));
+
+    // Optional: Add Axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+       .attr("transform", `translate(0,${height})`)
+       .call(xAxis);
+
+    svg.append("g")
+       .call(yAxis);
+  }
+</script>
+
+<svg width="600" height="400"></svg>
